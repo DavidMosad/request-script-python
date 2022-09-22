593b8dcb64fad3c0c19a074e51f34f05cfc1ba84
David <david.mosad@hotmail.fr>
2022-09-22 14:49:38 +0200

    top 2

--- script2.py
+++ script2.py
@@ -2,9 +2,11 @@ import requests
 import subprocess
 import time
 
-
+data = {
+    'vote': 'dogs'
+}
 url = 'http://votingappg2-testchargeg2.westus.cloudapp.azure.com'
-response = requests.post(url,params= "vote=Dogs")
+response = requests.post(url,data=data)
 print(response.headers)
 print(response.status_code)
 
