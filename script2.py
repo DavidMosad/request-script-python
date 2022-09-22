0c4f0a93cb6fc5ed9ba0b39025cb2ffc8da507cc
simplon-ThomasJulian <th.julian23@gmail.com>
2022-09-22 16:21:20 +0200

    json file

--- script2.py
+++ script2.py
@@ -14,8 +14,19 @@ print(response.status_code)
 print(response.headers['X-HANDLED-BY'])
 date = response.headers['Date']
 timestamp = time.mktime(datetime.datetime.strptime(date, "%a, %d %b %Y %H:%M:%S GMT").timetuple())
+xhandled = response.headers['X-HANDLED-BY']
+# Data to be written
+dictionary = {
+    xhandled : timestamp
+}
+ 
+# Serializing json
+json_object = json.dumps(dictionary, indent=4)
+ 
+# Writing to sample.json
+with open("sample.json", "x") as outfile:
+    outfile.write(json_object)
 
-
-data2 = {'X-HANDLED-BY'}
-with open("myfile.json", "w") as j:
-	json.dump(data2, j)
+# data2 = {['X-HANDLED-BY']}
+#with open("myfile.json", "w") as j:
+#	json.dump(data2, j)
