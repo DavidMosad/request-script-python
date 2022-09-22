375c21ec2ffb884a81b7ebec66b895ad3c7de653
David <david.mosad@hotmail.fr>
2022-09-22 15:00:39 +0200

    Quentin's help

--- script2.py
+++ script2.py
@@ -1,14 +1,14 @@
+import json
 import requests 
-import subprocess
-import time
 
 data = {
     'vote': 'dogs'
 }
 url = 'http://votingappg2-testchargeg2.westus.cloudapp.azure.com'
+
 response = requests.post(url,data=data)
 print(response.headers)
 print(response.status_code)
+print(response.headers['X-HANDLED-BY'])
 
-    
 
